231932a126c664ced36b95ff991e0f7a0607062b
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2026-04-01 20:06:26 +0200

    Time: 0 ms (100%), Space: 19.2 MB (94.88%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        k=0 
+        cur =  head
+
+        while cur:
+            k+=1
+            cur = cur.next
+        
+
+        dummy = ListNode(0, head)
+
+        cur = dummy
+        for _ in range(k-n):
+            cur = cur.next
+        
+        cur.next = cur.next.next
+
+        return dummy.next
