da0093ed50dc167becfc6f99e3c6856408dbfa2d
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2026-04-02 20:49:43 +0200

    Time: 399 ms (6.11%), Space: 137.5 MB (10.18%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,62 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        # initialize dummy head/tail
+        self.head = type("Node", (), {})()
+        self.tail = type("Node", (), {})()
+
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+    def removeNode(self, node):
+        prevNode, nextNode = node.prev, node.next
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+        node.next = node.prev = None
+
+    def insertNode(self, node):
+        prevNode = self.tail.prev
+        prevNode.next = node
+        node.prev = prevNode
+        node.next = self.tail
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.removeNode(node)      # FIX: add self
+        self.insertNode(node)      # FIX: add self
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.removeNode(node)
+            self.insertNode(node)
+        else:
+            if len(self.cache) == self.capacity:
+                lru = self.head.next
+                self.removeNode(lru)
+                del self.cache[lru.key]
+
+            # create node WITHOUT defining a class
+            node = type("Node", (), {})()
+            node.key = key
+            node.val = value
+            node.prev = node.next = None
+
+            self.cache[key] = node
+            self.insertNode(node)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
